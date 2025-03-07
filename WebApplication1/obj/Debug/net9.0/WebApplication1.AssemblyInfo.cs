256abbec3c94ee102fb9cc88d376e7387106a456
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2025-03-07 12:00:35 +0100

    packeges/done

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041d1ab4f1767596038739de5b5d1363ef97ed2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488515bc06962a08f5caf73abfacee5aca1bf474")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
